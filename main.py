a83070cfd8287b9aed6b2c34e84cbc5d9b91dfb3
liamk-zds <liamk@zetadataservices.com.au>
2026-03-29 12:51:43 +1100

    coming soon added. Stay comming sson online for now

--- main.py
+++ main.py
@@ -21,7 +21,14 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse(request, "index.html")
+    #return templates.TemplateResponse(request, "index.html")
+    return templates.TemplateResponse(request, "comint-soon.html")
+
+
+
+@app.get("/coming-soon", response_class=HTMLResponse)
+async def coming_soon(request: Request):
+    return templates.TemplateResponse(request, "coming-soon.html")
 
 
 @app.get("/developers", response_class=HTMLResponse)
